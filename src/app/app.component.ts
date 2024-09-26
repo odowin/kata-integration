c282a44bbb5ac169a4177e914e2cfdfc10a32e26
odowin <102875944+odowin@users.noreply.github.com>
2024-09-26 11:16:13 +0200

    With input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,48 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { CardComponent } from "./card/card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'kata-integration';
+
+  products = [
+    {
+      title: 'Produit 1',
+      description: 'Ceci est une courte description du produit.',
+      imageUrl: 'https://via.placeholder.com/150'
+    },
+    {
+      title: 'Produit 2',
+      description: 'Ceci est une courte description du produit.',
+      imageUrl: 'https://via.placeholder.com/150'
+    },
+    {
+      title: 'Produit 3',
+      description: 'Ceci est une courte description du produit.',
+      imageUrl: 'https://via.placeholder.com/150'
+    },
+    {
+      title: 'Produit 4',
+      description: 'Ceci est une courte description du produit.',
+      imageUrl: 'https://via.placeholder.com/150'
+    },
+    {
+      title: 'Produit 5',
+      description: 'Ceci est une courte description du produit.',
+      imageUrl: 'https://via.placeholder.com/150'
+    },
+    {
+      title: 'Produit 6',
+      description: 'Ceci est une courte description du produit.',
+      imageUrl: 'https://via.placeholder.com/150'
+    }
+  ];
 }
